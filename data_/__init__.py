ce3747d42039c1f015679ef65170f7e194af9bf4
Richard Neri <richardmatq@gmail.com>
2022-12-25 09:40:43 -0300

    Format

---
+++ data_/__init__.py
@@ -0,0 +1,38 @@
+from playsound import  PlaysoundException, playsound as playsound_  # noqa
+from vosk import Model, KaldiRecognizer, SetLogLevel  # noqa
+# Biblioteca de reconhecimento de fala Offline
+from pyttsx3 import speak as bot_say2 # Leitor de texto  # noqa
+from pyaudio import PyAudio, paInt16  # noqa
+from os import system, path, mkdir, startfile  # noqa
+from subprocess import run  # noqa
+from webbrowser import open_new  # noqa
+from random import choice  # noqa
+from pathlib import Path  # noqa
+from shutil import copy2  # noqa
+from gtts import gTTS # Leitor de texto do google  # noqa
+from sys import exit  # noqa
+import speech_recognition as sr  # noqa
+from googlesearch import search  # noqa
+
+
+DATADIR = Path(__file__).parent  / 'data_'  # noqa
+
+if (Path().absolute() / 'data_') != DATADIR:
+    DATADIR = Path(__file__).parent
+
+MEMORYDIR = Path(DATADIR) / 'memory.db'
+FULLLANGUAGE = Path(DATADIR) / 'full'
+# Link: https://alphacephei.com/vosk/models/vosk-model-pt-fb-v0.1.1-pruned.zip
+LIGHTLANGUAGE = Path(DATADIR) / 'lite'
+WARNINGSOUND = Path(DATADIR) / 'sound.wav'
+BOTVOICE = Path(DATADIR) / 'temp.mp3'
+
+# CACHEDIR = path.join(path.expanduser("~"), "AppData/Roaming/SaraCache")
+
+# if not path.isdir(CACHEDIR):
+#     mkdir(CACHEDIR)
+
+# copy2(WARNING, CACHEDIR)
+
+# WARNINGSOUND = Path(CACHEDIR) / 'sound.wav'
+# BOTVOICE = Path(CACHEDIR) / 'temp.mp3'
--- main.py
+++ main.py
@@ -1,57 +1,68 @@
 import sqlite3
-from data import *
+from abc import ABC, abstractmethod
+import data_
 
 
 class Brain(ABC):
 
     def __init__(self):
         self.status = True
-        self._farewell = ('Certo, até mais!', 'Até depois então', 'Falo com você mais tarde!', 'Estarei aqui!')
-        self._salutation = ('No que eu posso te ajudar?', 'Precisando de mim?', 'Chamou chamou', 'Estou aqui!')
+        self._farewell = (
+                'Certo, até mais!',
+                'Até depois então',
+                'Falo com você mais tarde!',
+                'Estarei aqui!'
+                )
+        self._salutation = (
+                'No que eu posso te ajudar?',
+                'Precisando de mim?',
+                'Chamou chamou',
+                'Estou aqui!'
+                )
 
     @property
     @abstractmethod
     def voice_detection(self): ...
 
-    def bot_say(self, text, dir_=BOTVOICE):
+    def bot_say(self, text, dir_=data_.BOTVOICE):
         try:
-            gTTS(text=text, lang='pt').save(dir_)
-            try: # Corrigi o bug de execução de som
-                playsound_(dir_)
-            except PlaysoundException:
-                system('cls')
-                playsound_(dir_)
+            data_.gTTS(text=text, lang='pt').save(dir_)
+            try:  # Corrigi o bug de execução de som
+                data_.playsound_(dir_)
+            except data_.PlaysoundException:
+                data_.system('cls')
+                data_.playsound_(dir_)
             dir_.unlink()
 
         except Exception:
-            system('cls')
-            bot_say2(text)
+            data_.system('cls')
+            data_.bot_say2(text)
 
-    def __learnig(self, DIR_=MEMORYDIR):
+    def __learnig(self, DIR_=data_.MEMORYDIR):
         self.bot_say('Me fale algo o que vou ouvir.')
         while True:
             asking = self.voice_detection
             if asking == '':
-                    continue
+                continue
             break
-            
+
         self.bot_say('Me fale o que devo responder')
         while True:
             answer = self.voice_detection
             if answer == '':
-                    continue
+                continue
             break
 
         memory = sqlite3.connect(DIR_)
         cursor = memory.cursor()
         try:
-            cursor.execute(f"INSERT INTO memory VALUES ('{asking}', '{answer}', '{answer}', '{answer}', '{answer}')")
+            cursor.execute(f"INSERT INTO memory VALUES ('{asking}', '{answer}', '{answer}', '{answer}', '{answer}')")  # noqa
             memory.commit()
             return 'Consegui aprender isso! Obrigada.'
         except sqlite3.OperationalError:
             return 'Já possuo essa informação em minha memória!'
 
-    def discourse(self, user_speech, DIR_=MEMORYDIR):
+    def discourse(self, user_speech, DIR_=data_.MEMORYDIR):
         memory = sqlite3.connect(DIR_)
         cursor = memory.cursor()
         cursor.execute('SELECT * FROM memory')
@@ -62,98 +73,99 @@ class Brain(ABC):
 
             if 'abrir' in user_speech:
                 search_ = "+".join(user_speech.split()[1:])
-                open_new(*[i for i in search(search_, stop=1, lang='br')])
+                data_.open_new(*[i for i in data_.search(search_, stop=1, lang='br')])  # noqa
                 return 'Abrindo...'
-            
+
             if 'pesquisar' in user_speech:
                 search_ = "+".join(user_speech.split()[1:])
-                open_new(f'https://www.google.com/search?q={search_}')
+                data_.open_new(f'https://www.google.com/search?q={search_}')
                 return 'Pesquisando...'
-            
+
             if user_speech == user_say:
-                return choice(bot_say)
-            
+                return data_.choice(bot_say)
+
         return 'Eu não entendi isso!'
 
 
 class BotSara(Brain):
-
     @property
-    def voice_detection(self): # Reconhecimento de fala Offline | from vosk import Model, KaldiRecognizer | import pyaudio
-        
+    def voice_detection(self):  # Reconhecimento de fala Offline
+        # from vosk import Model, KaldiRecognizer | import pyaudio
         # Speech Recognition
-        srrecognizer = sr.Recognizer() 
-        mic = sr.Microphone()
+        srrecognizer = data_.sr.Recognizer()
+        mic = data_.sr.Microphone()
 
-        # vosk   
-        SetLogLevel(-1)
+        # vosk
+        data_.SetLogLevel(-1)
         try:
-            model = Model(f'{FULLLANGUAGE}') # Carregar o Modelo de Idioma Cheio
+            model = data_.Model(f'{data_.FULLLANGUAGE}')
+            # Carregar o Modelo de Idioma Cheio
         except Exception:
-            system('cls')
-            model = Model(f'{LIGHTLANGUAGE}') # Carregar o Modelo de Idioma Leve
+            data_.system('cls')
+            model = data_.Model(f'{data_.LIGHTLANGUAGE}')
+            # Carregar o Modelo de Idioma Leve
 
-        recognizer = KaldiRecognizer(model, 16000) # Reconhecedor de Voz
+        recognizer = data_.KaldiRecognizer(model, 16000)  # Reconhecedor de Voz
 
-        try: # Corrigi o bug de execução de som
-            playsound_(WARNINGSOUND)
-        except PlaysoundException:
-            system('cls')
-            playsound_(WARNINGSOUND)
+        try:  # Corrigi o bug de execução de som
+            data_.playsound_(data_.WARNINGSOUND)
+        except data_.PlaysoundException:
+            data_.system('cls')
+            data_.playsound_(data_.WARNINGSOUND)
         except Exception:
             ...
-        
+
         timeout = 3
         while True:
-            system('cls')
-            try: # Speech Recognition
+            data_.system('cls')
+            try:  # Speech Recognition
                 with mic as source:
                     audio = srrecognizer.listen(source)
-                    speech = srrecognizer.recognize_google(audio, language='pt-BR').lower()
-                    system('cls')
-            except (sr.UnknownValueError, sr.RequestError): # Vosk
-                capture = PyAudio() # Capiturando o Mic
-                stream = capture.open(format=paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192) # Configurando Captura
-                stream.start_stream() # Iniciar reconhecimento
-                data = stream.read(16384) # Lendo os dados do mic | buffer de 16mb
+                    speech = srrecognizer.recognize_google(audio, language='pt-BR').lower()  # type: ignore # noqa
+                    data_.system('cls')
+            except (data_.sr.UnknownValueError, data_.sr.RequestError):  # Vosk
+                capture = data_.PyAudio()  # Capiturando o Mic
+                stream = capture.open(format=data_.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)  # Configurando Captura  # noqa
+                stream.start_stream()  # Iniciar reconhecimento
+                data = stream.read(16384)  # Lendo os dados do mic
 
-                if recognizer.AcceptWaveform(data): # Se identificar a voz
+                if recognizer.AcceptWaveform(data):  # Se identificar a voz
                     speech = eval(recognizer.Result())['text']
-            
-            system('cls')
+
+            data_.system('cls')
             try:
-                match speech:
+                match speech:  # type: ignore
                     case '' if timeout > 0:
                         timeout -= 1
                         if timeout == 0:
-                            self.status = True 
+                            self.status = True
                         continue
 
                     case 'sarah' | 'sara' | 'testando' if self.status:
                         self.status = False
-                        self.bot_say(choice(self._salutation))
+                        self.bot_say(data_.choice(self._salutation))
                         speech = self.voice_detection
 
                         if speech == '':
                             timeout = 3
                             continue
-        
+
                         return speech
-                    
-                    case 'vá dormir' | 'até mais' | 'até depois' if not self.status:
+
+                    case 'vá dormir' | 'até mais' | 'até depois' if not self.status:  # noqa
                         self.status = True
-                        self.bot_say(choice(self._farewell))
+                        self.bot_say(data_.choice(self._farewell))
 
                     case 'desligar':
                         self.bot_say('Tchau Tchau!')
                         exit(0)
 
                     case _ if not self.status:
-                        return speech
-                    
+                        return speech   # type: ignore
+
             except UnboundLocalError:
                 continue
-        
+
 
 if __name__ == '__main__':
     bot = BotSara()
